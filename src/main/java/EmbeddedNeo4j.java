ed3fac81b2af3d812e607ca6b82ebe7865ee74d7
Gabor Szarnyas <szarnyasg@gmail.com>
2017-12-11 20:11:51 -0500

    Try suggested workaround (not working)

--- src/main/java/EmbeddedNeo4j.java
+++ src/main/java/EmbeddedNeo4j.java
@@ -1,6 +1,5 @@
 import apoc.export.graphml.ExportGraphML;
 import org.neo4j.graphdb.GraphDatabaseService;
-import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 import org.neo4j.io.fs.FileUtils;
 import org.neo4j.kernel.api.exceptions.KernelException;
@@ -22,10 +21,8 @@ public class EmbeddedNeo4j {
 				.resolveDependency(Procedures.class);
 		proceduresService.registerProcedure(ExportGraphML.class);
 
-		try (Transaction t = graphDb.beginTx()) {
-			graphDb.execute("CALL apoc.import.graphml('my.graphml', {batchSize: 1000, useTypes: true})");
-			t.success();
-		}
+
+		graphDb.execute("CALL apoc.import.graphml('my.graphml', {batchSize: 1000, useTypes: true})").close();
 
 		System.out.println("Shutting down database ...");
 		graphDb.shutdown();
